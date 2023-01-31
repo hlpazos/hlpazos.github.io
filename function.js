b2340df95ee5e899693b70c32bc048f507cb1987
hlpazos <124065885+hlpazos@users.noreply.github.com>
2023-01-31 11:57:37 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,4 @@
+document.getElementById('botón').onclick = function () {
+    console.log("capture");
+    document.getElementById("demo").innerHTML = "Objetivo Laboral: avanzar en mi carrera profesional como analista financiera, utilizando mis habilidades y conocimientos presupuestarios de operaciones.";
+}
